4a3ed1ced3b3fe94ef62a86650d6bc4685149305
1085332096 <143300340+1085332096@users.noreply.github.com>
2024-05-05 09:37:04 -0500

    ColorPicker

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,9 +1,40 @@
+/* eslint-disable react/jsx-no-undef */
 import React from 'react'
+import {
+  GridComponent,
+  ColumnDirective,
+  ColumnsDirective,
+  Page,
+  Selection,
+  Edit,
+  Inject,
+  Toolbar,
+  Sort,
+  Filter
+} from '@syncfusion/ej2-react-grids'
+import { customersData, customersGrid } from '../data/dummy'
+import { Header } from '../components'
 
 function Customers () {
   return (
-    <div>Customers</div>
+    <div className='m-2 md:10 p-2 md:p-10 bg-white rounded-s-none-3xl'>
+      <Header category='Page' title='Customers' />
+      <GridComponent
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        toolbar={['Delete']}
+        editSettings={{ allowDeleting: true, allowEditing: true }}
+        width='auto'
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Toolbar, Filter, Sort, Selection, Edit]} />
+      </GridComponent>
+    </div>
   )
 }
-
 export default Customers
--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import {
+  RichTextEditorComponent,
+  Inject,
+  Image,
+  HtmlEditor,
+  Link,
+  QuickToolbar,
+  Toolbar
+} from '@syncfusion/ej2-react-richtexteditor'
+import { Header } from '../components'
+import { EditorData } from '../data/dummy'
 
 function Editor () {
   return (
-    <div>Editor</div>
+    <div className='m-2 md:10 p-2 md:p-10 bg-white rounded-s-none-3xl'>
+      <Header category='Page' title='Editor' />
+      <RichTextEditorComponent>
+        <EditorData />
+        <Inject
+          services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]}
+        />
+      </RichTextEditorComponent>
+    </div>
   )
 }
 
